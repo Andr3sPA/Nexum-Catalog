f16957571bed490fe35367bbb439e154acecf522
Gerardo Castillo <gcastillo.m14@gmail.com>
2025-07-21 10:45:57 -0500

    feat: v0.0.2

--- src/main/java/co/edu/udea/nexum/catalog/common/domain/exception/ForbiddenResourceAccessException.java
+++ src/main/java/co/edu/udea/nexum/catalog/common/domain/exception/ForbiddenResourceAccessException.java
@@ -2,7 +2,7 @@ package co.edu.udea.nexum.catalog.common.domain.exception;
 
 import static co.edu.udea.nexum.catalog.common.domain.utils.constants.CommonDomainConstants.FORBIDDEN_RESOURCE_ACCESS_ERROR_MESSAGE;
 
-public class ForbiddenResourceAccessException extends RuntimeException {
+public class ForbiddenResourceAccessException extends NexumException {
 
   public ForbiddenResourceAccessException() {
         super(FORBIDDEN_RESOURCE_ACCESS_ERROR_MESSAGE);
